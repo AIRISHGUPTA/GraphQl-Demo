9ba25979eecc80164f6c5533c5f6140ad9c3395d
airish.gupta <airish.gupta@paytmmoney.com>
2020-11-05 23:01:20 +0530

    eslint fix

--- server/mongoos-models/author.js
+++ server/mongoos-models/author.js
@@ -1,8 +1,8 @@
-const {Schema, model} = require("mongoose");
+const { Schema, model } = require('mongoose');
 
 const AuthorSchema = new Schema({
-    name: String,
-    age: Number,
-})
+  name: String,
+  age: Number,
+});
 
 module.exports = model('Author', AuthorSchema);
